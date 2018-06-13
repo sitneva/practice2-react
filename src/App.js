0d5dbb5492b44edf7259d9bf4bd4d5749136ba1f
sitneva <sitneva.t.v@gmail.com>
2018-06-13 18:37:29 +0300

    Show chars

--- src/App.js
+++ src/App.js
@@ -12,18 +12,21 @@ class App extends Component {
     inputChangeHandler = (event) => {
         this.setState({inputText: event.target.value})
     }
+    
 
 
 
   render() {
-        const CharList = this.state.inputText.split('');
+        const CharList = this.state.inputText.split('').map(smb =>{
+            return <Char symbol={smb}></Char>
+        });
 
     return (
       <div className="App">
         <input type="text" onChange={(event) => this.inputChangeHandler(event)}/>
           <p>{this.state.inputText}</p>
           <Validation lenght={this.state.inputText.length}></Validation>
-          <Char symbol="a"></Char>
+          {CharList}
       </div>
     );
   }
